ec986cb74fc59461333da69be1c7b68cf0df9b1a
Piotr <piotr.gaszczyk@gmail.com>
2024-05-18 09:33:13 +0200

    python_v5_simple

---
+++ hangman_simple.py
@@ -0,0 +1,44 @@
+import os
+
+def display_current_state(word, guessed_letters):
+    return " ".join([letter if letter in guessed_letters else "_" for letter in word])
+
+def get_guess(already_guessed):
+    while True:
+        guess = input("Odgadnij literę: ").lower()
+        if guess == "quit":
+            print("Dziękujemy za grę! Do zobaczenia!")
+            exit()
+        elif len(guess) != 1 or not guess.isalpha():
+            print("Proszę wpisać jedną literę.")
+        elif guess in already_guessed:
+            print("Ta litera została już odgadnięta. Spróbuj ponownie.")
+        else:
+            return guess
+
+def hangman(word, lives=6):
+    guessed_letters = set()
+    wrong_guesses = set()
+
+    while lives > 0:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Stan gry: ", display_current_state(word, guessed_letters))
+        print("Błędne litery: ", " ".join(wrong_guesses))
+        print(f"Pozostałe życia: {lives}")
+
+        guess = get_guess(guessed_letters | wrong_guesses)
+
+        if guess in word:
+            guessed_letters.add(guess)
+            if all(letter in guessed_letters for letter in word):
+                print(f"Gratulacje! Odgadłeś słowo: {word}")
+                break
+        else:
+            wrong_guesses.add(guess)
+            lives -= 1
+
+    if lives == 0:
+        print(f"Przegrałeś! Słowo to: {word}")
+
+if __name__ == "__main__":
+    hangman("python")
